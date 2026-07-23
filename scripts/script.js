362eed9a8ea312c1d81d6839a14eebd3c2d72bf8
theabidaly <theabidaly@gmail.com>
2026-07-22 22:55:47 -0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,7 @@ function onLoad()
     input = document.getElementById("input-text")
     text = document.getElementById("text")
     
+    toggleInformation();
 
     submit_button.addEventListener("click", function ()
     {
@@ -47,6 +48,20 @@ function onLoad()
     })
 }
 
+// hide/unhide information
+function toggleInformation(willCheck, checkedVal)
+{
+    a = willCheck || false;
+    b = checkedVal || false;
+    info = document.getElementsByClassName("info")
+
+    for(element of info)
+    {
+        if(willCheck && element.getAttribute("hidden") != checkedVal) continue;
+        element.toggleAttribute("hidden")
+    }
+}
+
 // get text from input text return nothing if blank
 function getUsername()
 {
@@ -57,10 +72,31 @@ function getUsername()
     return document.getElementById("input-text").value;
 }
 
+//convert seconds to minutes/hours
+function convertTimeFromSeconds(time)
+{
+    if(time < 100)
+        return [time, "sec"]
+    if(time < 6000)
+        return [Math.floor(time/60), "min"]
+    return[Math.floor(time/3600), "hrs"]
+}
+function getLifetime(time)
+{
+    elapsedTime = Date.now() - time;
+    return Math.floor(elapsedTime/86400000)
+}
+function getActivity(playtime, lifespan)
+{
+    lifespan = lifespan/1000
+    return Math.round((playtime/lifespan)*100000)/1000
+}
+
 // call api
 async function apiCall()
 {
     const chip1 = "njtZSkJgp23li7=LPO3S1vvJ"
+    json_information = null
 
     try
     {
@@ -80,6 +116,20 @@ async function apiCall()
     }
 
     console.log(json_information)
-}
 
-//
+    toggleInformation(true, false)
+
+    playtime = document.getElementById("playtime")
+    playtimeUnit = document.getElementById("playtime-unit")
+    lifetime = document.getElementById("lifetime")
+    bulletRating = document.getElementById("bullet-rating")
+    blitzRating = document.getElementById("blitz-rating")
+    rapidRating = document.getElementById("rapid-rating")
+
+    playtime.innerHTML = convertTimeFromSeconds(json_information.playTime.total)[0]
+    playtimeUnit.innerHTML = convertTimeFromSeconds(json_information.playTime.total)[1]
+    lifetime.innerHTML = getLifetime(json_information.createdAt)
+    bulletRating.innerHTML = json_information.perfs.bullet.rating
+    blitzRating.innerHTML = json_information.perfs.blitz.rating
+    rapidRating.innerHTML = json_information.perfs.rapid.rating
+}
